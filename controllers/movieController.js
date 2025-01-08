43161abbc1da667b87a3e0220ca4ef7de4d94b43
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-08 16:16:26 +0100

    ADD index and show functions in movieController

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,26 @@
 const connection = require('../data/db.js');
 
 function index(_, res) {
-    console.log('hello world!');
+    const sql = 'SELECT * FROM posts';
+
+    connection.query(sql, (err, results) => {
+        if (err)
+            return res.status(500).json({ error: 'Database query failed' });
+        res.json(results);
+    });
 }
 
 function show(req, res) {
-    console.log('hello world!');
+    const id = req.params.id;
+    console.log(id);
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+    connection.query(sql, [id], (err, results) => {
+        if (err)
+            return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0)
+            return res.status(404).json({ error: 'Movie not found' });
+        res.json(results[0]);
+    });
 }
 
 module.exports = { index, show };

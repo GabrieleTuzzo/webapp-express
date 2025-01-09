08dfb399765b210e561ee69be03b634154379d8f
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-01-09 09:38:02 +0100

    FIX errorHandler added next in function parameters

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 app.use('/movies', moviesRouter);
 
-app.use(notFound);
 app.use(errorHandler);
+app.use(notFound);
 
 app.listen(process.env.PORT, function () {
     console.log('Server started on port ' + process.env.PORT);
